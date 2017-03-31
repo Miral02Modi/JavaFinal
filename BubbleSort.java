28a4c1398fb2e6460e145deda0ef163626ea9ce7
Miral <miralmodi07@gmail.com>
2017-03-31 12:28:15 +0530

    31/03/2017

---
+++ BubbleSort.java
@@ -0,0 +1,10 @@
+ class BubbleSort
+{
+   public static void main(String args[])
+   {
+	int arr[] = {1,4,3,5,2};
+	arr = Util.bubbleSortInt(arr)
+	for(int i =0 ; i < ch	.length ;  i++)
+	    System.out.println(ch[i]); 		
+   }	
+}		
