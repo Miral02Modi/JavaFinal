9a268cd275ae6b6ac11c0b34d16cf46d19dfc89c
Miral <miralmodi07@gmail.com>
2017-04-07 09:50:14 +0530

    Regex

---
+++ RegularExpression/src/Regex.java
@@ -0,0 +1,54 @@
+import java.math.MathContext;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+	public static void main(String[] args) {
+
+		Pattern p = Pattern.compile(".<<");// . represents single character
+		Matcher m = p.matcher("a<<");
+		boolean b = m.matches();
+
+		String mstr = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxxx."
+				+ "Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016. ";
+
+		Scanner sc = new Scanner(System.in);
+		Scanner scr = new Scanner(System.in);
+		boolean flag=true;
+			
+			System.out.print("Enter The your name::");
+			String name = sc.next();
+			Pattern pattern = Pattern.compile("<<name>>");
+			Matcher matcher = pattern.matcher(mstr);
+			if(matcher.find()){
+				mstr=mstr.replace(mstr.substring(matcher.start(), matcher.end()), name);
+			}
+			
+			pattern = Pattern.compile("<<full name>>");
+			matcher = pattern.matcher(mstr);
+			System.out.print("Enter The full name::");
+			name = scr.nextLine();
+			if(matcher.find()){
+				mstr=mstr.replace(mstr.substring(matcher.start(), matcher.end()), name);
+			}
+			
+			System.out.print("Enter your number::");
+			pattern = Pattern.compile("xxxxxxxxxx");
+			name = scr.next();
+			matcher = pattern.matcher(mstr);
+			if(matcher.find()){
+				mstr=mstr.replace(mstr.substring(matcher.start(), matcher.end()), name);
+			}
+			
+			System.out.print("Enter your Date::");
+			pattern = Pattern.compile("01/01/2016");
+			name = scr.next();
+			matcher = pattern.matcher(mstr);
+			if(matcher.find()){
+				mstr=mstr.replace(mstr.substring(matcher.start(), matcher.end()), name);
+				System.out.println(mstr);
+			}
+	}
+}
