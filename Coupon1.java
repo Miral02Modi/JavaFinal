56c38e354b57fd0ea74d2212dcb887d900b7068b
Miral <miralmodi07@gmail.com>
2017-04-07 09:43:22 +0530

    Coupen

--- Coupon1.java
+++ Coupon1.java
@@ -1,37 +1,33 @@
+import java.util.Scanner;
 
-import java.util.*;
-
-class Coupon1 {
-
-	public static void main(String args[]) {
+public class Coupon1 {
+	public static void main(String[] args) {
 		
+		int count=0;
+		int numCoup;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("ENter The  Coupoun");
-		int numCoup = sc.nextInt();
-		System.out.println("Enter The coupen Range");
-		int range = sc.nextInt(), i = 0;
-		
-		double arr[] = new double[numCoup];
-		Random ar = new Random();
-		int gen,count=0,count1=0;
-		int gen1=0;
-
-		while (i < numCoup) {
-			boolean flag = true;				
-			gen1 = (int)Math.round(Math.random() * numCoup + 1) ;
-			count1++;
-			for (int j = 0; j < arr.length; j++) {
-				if (arr[j] == gen1){
-					flag = false;  
+		System.out.println("Enter The value::");
+		numCoup = sc.nextInt();
+		int arr[] = new int[numCoup];
+		int count1=0;
+		Random rm = new Random(); 
+		//System.out.println(arr.length);
+		for (int i = 1; count1 != numCoup; i++) {
+			boolean flag=true;
+			int gen = (int)Math.round(Math.random() * numCoup ) +1 ;
+			for(int j=0;j<arr.length;j++) {
+				if (arr[j] == gen){
+					flag=false;
 					break;
 				}
 			}
-			if (flag) {
-				arr[count++] = gen1 ;
-				System.out.println(gen1);
-				i++;
-			}
+			if(flag){
+				System.out.println(gen);
+				arr[count1++] = gen; 
+			}	
+			count++;
 		}
-		System.out.println("Count is::"+count1);
+		System.out.println("Count is::"+count);
 	}
 }
+
