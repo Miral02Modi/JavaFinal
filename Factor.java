91d8e81e7c59245319ea17ae280f10bbf8b8c890
Miral <miralmodi07@gmail.com>
2017-04-07 10:08:40 +0530

    Factor

--- Factor.java
+++ Factor.java
@@ -1,26 +1,35 @@
-import java.util.*;
-class  Factor{
-   public static void main(String args[]){
-        int prime=2;
-        int count=0;
+import java.util.Scanner;
+public class Factor {
 
-	Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number of Prime factor you Want");
-	count= sc.nextInt();
-
-	while(count > 0){
-	   int i=2;boolean flag=true;
-	   while(prime > i ){
-           	if(prime % i == 0){
-		   flag=false;
+	public static boolean chckPrime(int num) {
+		boolean flag = true;
+		for (int i = 2; i <= num / 2; i++) {
+			if (num % i == 0) {
+				flag = false;
+				break;
+			}
 		}
-		i++;
- 	   }
-	    if(flag){
-	     System.out.println("Prime Factor is::"+prime);
-              count--;
-	    }
-	  prime++;
+		return flag;
 	}
-   }
+
+	public static boolean checkFact(int multiple, int multiplier) {
+		return multiple % multiplier == 0;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the integer number");
+		if (scn.hasNextInt()) {
+			int N = scn.nextInt();
+			System.out.println("Prime factors for given number are :");
+			for (int i = 2; i <= N / 2; i++) {
+				if ((checkFact(N, i)) && (chckPrime(i))) {
+					System.out.print(i + "\t");
+				}
+			}
+		} else {
+			System.err.println("Enter integers only");
+		}
+		scn.close();
+	}
 }
